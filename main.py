c5c78b21e985fa646e35702929b030f8c67a3e80
Harley-Squires <harleysquires1@gmail.com>
2024-04-27 05:50:32 -0500

    Included wordcount

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         print(file_contents)
+    words = file_contents.split()
+    print(len(words))
 main()
